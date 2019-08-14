3289849c41ed0c9157913ec693c279a38927d7f6
cainsyake <cainsyake@qq.com>
2019-08-14 18:04:36 +0800

    feat(login): 跳转至登陆页

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,63 @@
 import 'package:flutter/material.dart';
+import 'views/login.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: 'Flutter Tutorial',
-    home: new TutorialHome(),
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
   ));
 }
 
-class TutorialHome extends StatelessWidget {
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //Scaffold是Material中主要的布局组件.
-    return new Scaffold(
-      appBar: new AppBar(
-        leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: new Text('标题栏1'),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
       ),
-      //body占屏幕的大部分
-      body: new Center(
-        child: new Column(
+      body: Center(
+        child: Column(
           children: [
-            new Text('Hello, wu bobo!'),
-            new Text('Hello, wu bobo2'),
-            new Row(
-              children: [
-                new Text('left'),
-                new Text('right')
-              ],
+            RaisedButton(
+              child: Text('Open route'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                );
+              },
             ),
-            new MyCounter(),
+            RaisedButton(
+              child: Text('跳转至登录页'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+              },
+            ),
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: new Icon(Icons.add),
-        onPressed: null,
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
       ),
-    );
-  }
-}
-
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({this.count});
-
-  final int count;
-  @override
-  Widget build(BuildContext context) {
-    return new Text('count:$count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({this.onPressed});
-  final VoidCallback onPressed;
-  @override
-  Widget build(BuildContext context) {
-    return new RaisedButton(
-      onPressed: onPressed,
-      child: new Text('点这里加1'),
-    );
-  }
-}
-
-class MyCounter extends StatefulWidget {
-  @override
-  _MyCounterState createState() => new _MyCounterState();
-}
-
-class _MyCounterState extends State<MyCounter> {
-  int _counter = 0;
-
-  void _operate() {
-    setState(() {
-      _counter++;
-    });
-
-    print(_counter);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Column(
-      children: <Widget>[
-        new CounterIncrementor(onPressed: _operate),
-        new CounterDisplay(count: _counter,),
-      ],
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
     );
   }
 }
