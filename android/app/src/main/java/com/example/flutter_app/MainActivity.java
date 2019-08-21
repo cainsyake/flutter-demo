4708470edbd11747735df11ccb88fa330d0ba5d9
cainsyake <cainsyake@qq.com>
2019-08-21 10:42:58 +0800

    feat(local-auth): 本地生物认证

--- android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.flutter_app;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+//import io.flutter.app.FlutterActivity;
+import io.flutter.app.FlutterFragmentActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
--- lib/views/login/localAuth.dart
+++ lib/views/login/localAuth.dart
@@ -5,6 +5,9 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+
 
 class LocalAuth extends StatefulWidget {
   @override
@@ -14,11 +17,92 @@ class LocalAuth extends StatefulWidget {
 }
 
 class _LocalAuthState extends State<LocalAuth> {
+
+  /// 本地认证框架
+  final LocalAuthentication auth = LocalAuthentication();
+  /// 是否有可用的生物识别技术
+  bool _canCheckBiometrics;
+  /// 生物识别技术列表
+  List<BiometricType> _availableBiometrics;
+  /// 识别结果
+  String _authorized = '验证失败';
+
+  /// 检查是否有可用的生物识别技术
+  Future<Null> _checkBiometrics() async {
+    bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
+    });
+  }
+  /// 获取生物识别技术列表
+  Future<Null> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _availableBiometrics = availableBiometrics;
+    });
+  }
+  /// 生物识别
+  Future<Null> _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticateWithBiometrics(
+          localizedReason: '扫描指纹进行身份验证',
+          useErrorDialogs: true,
+          stickyAuth: false);
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _authorized = authenticated ? '验证通过' : '验证失败';
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('本地权限校验'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('是否有可用的生物识别技术: $_canCheckBiometrics\n'),
+          RaisedButton(
+            child: const Text('检查生物识别技术'),
+            onPressed: _checkBiometrics,
+          ),
+          Text('可用的生物识别技术: $_availableBiometrics\n'),
+          RaisedButton(
+            child: const Text('获取可用的生物识别技术'),
+            onPressed: _getAvailableBiometrics,
+          ),
+          Text('状态: $_authorized\n'),
+          RaisedButton(
+            child: const Text('验证'),
+            onPressed: _authenticate,
+          )
+        ],
       ),
     );
   }
